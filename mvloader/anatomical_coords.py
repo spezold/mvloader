969c32b378b51aab698cc9c0d5d86ac2a9aa3bcb
Simon Pezold <simon.pezold@unibas.ch>
2018-02-13 12:28:09 +0100

    minor

--- mvloader/anatomical_coords.py
+++ mvloader/anatomical_coords.py
@@ -25,11 +25,11 @@ def permutation_matrix(src, dst):
     Returns
     -------
     tuple
-        A two-tuple ``(src2dst, dst2src)`` where ``src2dst`` holds the permutation and reflection matrix that maps
-        coordinates from the ``src`` system to the ``dst`` system, and ``dst2src`` holds the matrix for the inverse
-        mapping. Both :math:`3x3` matrices are Numpy arrays with a determinant of plus/minus one, containing only
-        zeros, ones, and minus ones. A minus one signifies a swapped axis direction (e.g. "L" in ``src`` becomes "R" in
-        ``dst``).
+        A two-tuple ``(src2dst, dst2src)`` where ``src2dst`` holds the :math:`3x3` permutation and reflection matrix
+        that maps coordinates from the ``src`` system to the ``dst`` system, and ``dst2src`` holds the :math:`3x3`
+        matrix for the inverse mapping. Both matrices are Numpy arrays with a determinant of plus/minus one,
+        containing only zeros, ones, and minus ones. A minus one signifies a swapped axis direction (e.g. "L" in
+        ``src`` becomes "R" in ``dst``).
     """
     src = src.upper()
     dst = dst.upper()
