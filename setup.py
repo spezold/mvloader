e7dff900f5f23068bc50fa2c0074724f97cc5717
Simon Pezold <simon.pezold@unibas.ch>
2018-03-22 11:46:29 +0100

    pydicom version handling -> new version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="mvloader",
-    version="0.2.201803211222",
+    version="0.2.201803221138",
     description="load medical image volumes (3D)",
     long_description="See readme.md in the project folder or on the github page.",
     author="Simon Pezold",
