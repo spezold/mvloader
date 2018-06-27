f0daa93d23c9f0130d931ba52429697a827b9d4c
Simon Pezold <simon.pezold@unibas.ch>
2018-06-27 18:41:04 +0200

    point to readme for project overview

--- mvloader/__init__.py
+++ mvloader/__init__.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+See the project's "readme" file [MVLOADER1]_ for an overview of MVloader's capabilities.
+
+References
+----------
+.. [MVLOADER1] https://github.com/spezold/mvloader/blob/master/readme.md (20180627)
+"""
+
 import pkg_resources
 
 # Following one of the recipes in [*]_ (probably not the best one, though)
